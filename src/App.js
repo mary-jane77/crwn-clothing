2b76e25049901976e68252bf39540815131d89c2
Elizabeth Tukaeva <elizabeth77531@gmail.com>
2021-02-07 18:44:56 +0300

    feat: create a project with create-react-app, add styling

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './pages/homepage/homepage.styles.scss';
+
+import HomePage from './pages/homepage/homepage.component';
+
+function App() {
+  return (
+    <div>
+    <HomePage />
+    </div>
+  );
+}
+
+export default App;
