75fa4dddf2abb81a8a57c64088439c6e5c4cff49
Elizabeth Tukaeva <elizabeth77531@gmail.com>
2021-02-10 10:51:45 +0300

    feat: add header

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component'
 
 function App() {
   return (
     <Router>
       <div>
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
