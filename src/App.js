190e88206a0ee5e8e1e42da82470a635b55208cb
Elizabeth Tukaeva <elizabeth77531@gmail.com>
2021-02-25 23:39:27 +0300

    feat: add redirect and user action type

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Switch, Route, Redirect, BrowserRouter as Router } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import  { setCurrentUser } from './redux/user/user.actions'
+import { setCurrentUser } from './redux/user/user.actions';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 function App() {
+  const currentUser = useSelector((state) => state.user.currentUser);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -33,17 +33,17 @@ function App() {
   }, []);
 
   return (
-    
-      <Router>
-        <div>
-          <Header />
-          <Switch>
-            <Route exact path='/' component={HomePage} />
-            <Route path='/shop' component={ShopPage} />
-            <Route path='/signIn' component={SignInAndSignUpPage} />
-          </Switch>
-        </div>
-      </Router>
+
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signIn' component={() => currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
