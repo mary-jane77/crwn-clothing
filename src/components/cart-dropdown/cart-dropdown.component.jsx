9ba12a4472b92f0ae227fbc568d401ce2e8f79b6
Elizabeth Tukaeva <elizabeth77531@gmail.com>
2021-02-26 23:14:08 +0300

    feat: add cart items in dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import CustomButton from '../сustom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => (
-    <div className='cart-dropdown'>
-        <div className='cart-items' />
-        <CustomButton >GO TO CHECKOUT</CustomButton >
-    </div>
-);
+const CartDropdown = () => {
+    const cartItems = useSelector((state) => state.cart.cartItems);
+    
+    return (
+        <div className='cart-dropdown'>
+            <div className='cart-items'>{cartItems.map(cartItem => (
+                <CartItem key={cartItem.id} item={cartItem} />
+              ))}
+            </div>
+            <CustomButton >GO TO CHECKOUT</CustomButton >
+        </div>
+    )
+};
 
 export default CartDropdown;
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import CustomButton from '../сustom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
+
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-    <div className='collection-item'>
-        <div 
-        className='image'
-        style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-        >
-            
-        </div>
-        <div className='collection-footer'>
+const CollectionItem = ({ item }) => {
+    const dispatch = useDispatch();
+    const { name, price, imageUrl } = item;
+    return (
+        <div className='collection-item'>
+            <div
+                className='image'
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            >
+
+            </div>
+            <div className='collection-footer'>
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
-    </div>
-);
+            <CustomButton onClick={()=>dispatch(addItem(item))} inverted> Add to cart</CustomButton>
+        </div>
+    );
+};
 
 export default CollectionItem;
