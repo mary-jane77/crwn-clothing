1f761ac19574e453fcc6c25100feb8af1e437ced
Elizabeth Tukaeva <elizabeth77531@gmail.com>
2021-02-26 20:12:38 +0300

    feat: add cart icon and cart dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,37 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 const Header = () => {
-
+    const dispatch = useDispatch();
     const currentUser = useSelector((state) => state.user.currentUser);
+    const hidden = useSelector((state) => state.cart.hidden);
 
     return (
-    <div div className='header' >
-        <Link to='/' className='logo-container'>
-            <Logo className='logo' />
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
-                SHOP
+        <div div className='header' >
+            <Link to='/' className='logo-container'>
+                <Logo className='logo' />
             </Link>
-            <Link className='option' to='/shop'>
-                CONTACT
+            <div className='options'>
+                <Link className='option' to='/shop'>
+                    SHOP
             </Link>
+                <Link className='option' to='/shop'>
+                    CONTACT
+            </Link>
+                {
+                    currentUser ?
+                        <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                        :
+                        <Link className='option' to='/signin'>SIGN IN</Link>
+                }
+                <CartIcon onClick={() => dispatch(toggleCartHidden)} />
+            </div>
             {
-                currentUser ?
-                    <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-                    :
-                    <Link className='option' to='/signin'>SIGN IN</Link>
+                hidden? null: <CartDropdown />
             }
-        </div>
-    </div >
+            
+        </div >
     );
 };
 
